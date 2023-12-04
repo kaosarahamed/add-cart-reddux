ac9a72fa66cd4540a12bcd6d44477635f84519ed
kaosar ahamed <kaocar07@gmail.com>
2023-12-04 16:08:55 +0600

    commit all files

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,46 +1,53 @@
 import { useSelector } from "react-redux";
-import "../App.css"
-import Logo from "../assets/logo.png"
+import "../App.css";
+import Logo from "../assets/web-app-logo-white.png";
 import PropTypes from "prop-types";
 
-
-
-
-
 function Header(props) {
-
   const handleCart = () => {
     props.setIsshown(false);
-    props.setDivShow(true)
-  }
+    props.setDivShow(true);
+  };
 
   const handleDiv = () => {
     props.setIsshown(true);
-    props.setDivShow(false)
-  }
+    props.setDivShow(false);
+  };
 
-
-  const price = useSelector(state => state.price);
-    return (
-        <nav className="bg-[#171C2A] py-4">
-    <div className="navBar">
-      <a href="#">
-        <img src={Logo} alt="LWS" className="max-w-[140px]" />
-      </a>
-
-      <div className="flex gap-4">
-        <a href="#" className="navHome" id="lws-home" onClick={() => handleDiv()}> Home </a>
-        <a href="#" className="navCart" id="lws-cart" onClick={() => handleCart()}>
-          <i className="text-xl fa-sharp fa-solid fa-bag-shopping"></i>
-          <span id="lws-totalCart">{price.quantity}</span>
+  const price = useSelector((state) => state.price);
+  return (
+    <nav className="bg-[#171C2A] py-4">
+      <div className="navBar">
+        <a href="#">
+          <img src={Logo} alt="LWS" className="img" />
         </a>
+
+        <div className="flex gap-4">
+          <a
+            href="#"
+            className="navHome"
+            id="lws-home"
+            onClick={() => handleDiv()}
+          >
+            {" "}
+            Home{" "}
+          </a>
+          <a
+            href="#"
+            className="navCart"
+            id="lws-cart"
+            onClick={() => handleCart()}
+          >
+            <i className="text-xl fa-sharp fa-solid fa-bag-shopping"></i>
+            <span id="lws-totalCart">{price.quantity}</span>
+          </a>
+        </div>
       </div>
-    </div>
-  </nav>
-    )
+    </nav>
+  );
 }
 Header.propTypes = {
-  setIsshown : PropTypes.any,
-  setDivShow : PropTypes.any
-}
-export default Header
+  setIsshown: PropTypes.any,
+  setDivShow: PropTypes.any,
+};
+export default Header;
--- src/redux/products/initialState.jsx
+++ src/redux/products/initialState.jsx
@@ -1,21 +1,49 @@
 const initialState = [
-    {
-        id : 1,
-        title : "Spring and summershoes",
-        image : "https://media.istockphoto.com/id/1469859927/photo/portrait-of-attractive-asian-woman-in-a-park.jpg?s=1024x1024&w=is&k=20&c=j74j6UCO2d07O9ZIB3YAYWMRWJtZjBG-yKuulS8NDMk=",
-        category : "Mens Shoes",
-        price : 400,
-        qty : 5
-    },
-    {
-        id : 2,
-        title : "Spring and new shoes collections",
-        image : "https://media.istockphoto.com/id/1469859927/photo/portrait-of-attractive-asian-woman-in-a-park.jpg?s=1024x1024&w=is&k=20&c=j74j6UCO2d07O9ZIB3YAYWMRWJtZjBG-yKuulS8NDMk=",
-        category : "Mens Juta",
-        price : 500,
-        qty : 5
-    },
+  {
+    id: 1,
+    title: "Spring and summershoes",
+    image:
+      "https://media.istockphoto.com/id/1469859927/photo/portrait-of-attractive-asian-woman-in-a-park.jpg?s=1024x1024&w=is&k=20&c=j74j6UCO2d07O9ZIB3YAYWMRWJtZjBG-yKuulS8NDMk=",
+    category: "Mens Shoes",
+    price: 400,
+    qty: 5,
+  },
+  {
+    id: 2,
+    title: "Spring and new shoes collections",
+    image:
+      "https://www.zdnet.com/a/img/resize/df0442de58d37347fe696c1f68b3beab3c798b19/2023/04/24/4e586f53-afa2-452d-baf4-cc7c78c2c5fb/samsung-galaxy-a54-5g.jpg?auto=webp&width=1280",
+    category: "Mens Juta",
+    price: 500,
+    qty: 5,
+  },
+  {
+    id: 3,
+    title: "nexg N8 Walton Phone",
+    image:
+      "https://waltonbd.com/image/cache/catalog/mobile/smart-phone/nexg-n8/id-n8/nexg-n8_id1-364x364.jpg",
+    category: "Mens Juta",
+    price: 1000,
+    qty: 5,
+  },
+  {
+    id: 4,
+    title: "nexg N6 Walton Phone",
+    image:
+      "https://waltonbd.com/image/cache/catalog/mobile/smart-phone/nexg-n6/n6-id/nexg-n6_id3-364x364.jpg",
+    category: "Mens Juta",
+    price: 1200,
+    qty: 5,
+  },
+  {
+    id: 5,
+    title: "nexg N6 Walton Phone",
+    image:
+      "https://waltonbd.com/image/cache/catalog/mobile/smart-phone/orbit-y70/id-y70/update/orbit-y70_id1-364x364.jpg",
+    category: "Mens Juta",
+    price: 1500,
+    qty: 5,
+  },
 ];
-
 
 export default initialState;
